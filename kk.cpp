5710b1aeaa0c09903717e83a4c5fedb5a056500a
Kristine Zhang <Kristine@Kristines-MacBook-Air-2.local>
2017-04-15 01:37:10 -0400

    meh

---
+++ kk.cpp
@@ -0,0 +1,79 @@
+int max_iter = 25000
+
+int main(int argc, char *argv[]) {
+
+  // read in input from user
+  if (argc != 2) {
+    cout << "Failure: incorrect number of arguments\n";
+    return 1;
+  }
+
+  // open and read inputfile
+
+  // find and return result of running kk
+
+  // generate 100 random instances
+
+    // for each, find the result using 7 different methods
+
+  return 0;
+}
+
+void karkarp(){
+
+  // use mergesort to sort the vector
+
+  // while the size is greater than 1,
+
+    // find the difference between the two greatest elements
+
+    // place this difference at the
+
+}
+
+// temporarily written assuming the standard representation
+void reprand(){
+  s = makerand_standard();
+  for (int i = 0; i < max_iter; i++){
+    s_new = makerand_standard();
+    if (residue_standard(s_new) < residue_standard(s)){
+      s = s_new;
+    }
+  }
+
+  return s;
+}
+
+void climbing(){
+
+}
+
+void annealing(){
+
+}
+
+void makerand_standard(){
+
+
+}
+
+void neighbor_standard(){
+
+
+}
+
+void residue_standard(){
+
+}
+
+void makerand_prepart(){
+
+}
+
+void neighbor_prepart(){
+
+}
+
+void residue_prepart(){
+
+}
