3e0884fa87017dc9d5d95866323d83030df5ad15
Kristine Zhang <Kristine@Kristines-MacBook-Air-2.local>
2017-04-14 21:40:07 -0400

    structure

---
+++ partition.cpp
@@ -0,0 +1,41 @@
+
+int main(){
+
+
+
+  return 0;
+}
+
+void karkarp(){
+
+}
+
+void reprand(){
+
+}
+
+void climbing(){
+
+}
+
+void annealing(){
+
+}
+
+void makerand_standard(){
+
+
+}
+
+void neighbor_standard(){
+
+
+}
+
+void makerand_prepart(){
+
+}
+
+void neighbor_prepart(){
+
+}
